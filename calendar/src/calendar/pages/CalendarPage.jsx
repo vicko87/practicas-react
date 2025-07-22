526ee8ffb7a2e8857bd50d8edad06df00faf67b1
Victoria <vkolomytseva87@gmail.com>
2025-07-22 16:35:16 +0200

    react big calendar

--- calendar/src/calendar/pages/CalendarPage.jsx
+++ calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,46 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import enUS from 'date-fns/locale/en-US'
 import { Navbar } from "../components/Navbar"
+import {addHours, format,  parse, startOfWeek, getDay} from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Victoria'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
    <Navbar />
+
+   <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc(100vh - 80px)' }}
+    />
+
     </>
   )
 }
