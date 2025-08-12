7015b84ab334fd379c5d174aba3a65199affa04e
Victoria <vkolomytseva87@gmail.com>
2025-08-12 17:32:26 +0200

    creando rutas de usuarios

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,1 +1,24 @@
-console.log('Hola mundo')
+const express = require('express');
+require('dotenv').config();
+
+
+
+
+// Crear el servidor de express
+const app = express();
+
+//Directorio Publico
+app.use(express.static('public'));
+
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+
+
+
+
+
+//Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+})
