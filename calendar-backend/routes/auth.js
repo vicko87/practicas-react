00cc17e8ff3046fb405a392d105f820f7d9ef7a1
Victoria <vkolomytseva87@gmail.com>
2025-08-13 11:17:26 +0200

    edpoints de remover crear y login

--- calendar-backend/routes/auth.js
+++ calendar-backend/routes/auth.js
@@ -2,15 +2,20 @@
 // host + /api/auth
 
 const {Router} = require('express');
-const router = Router()
+const router = Router();
+
+const {crearUsuario, loginUsuario, revalidarToken} = require('../controllers/auth');
+
+router.post('/new',  crearUsuario );
+
+
+router.post('/', loginUsuario);
 
 
 
-router.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-});
+router.get('/renew', revalidarToken);
+
+
 
 
 module.exports = router;
