24362bf8e7b535547ca184efe6019f0759faea00
Victoria <vkolomytseva87@gmail.com>
2025-08-13 12:28:58 +0200

    recuperar informacion de un posteo

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -10,6 +10,8 @@ const app = express();
 //Directorio Publico
 app.use(express.static('public'));
 
+//Lectura y parseo del body 
+app.use(express.json())
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
