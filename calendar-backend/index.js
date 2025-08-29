c9d855e0974b23bbb8d57884444acfa5ef04c97a
Victoria <vkolomytseva87@gmail.com>
2025-08-29 17:45:16 +0200

    app calendarapp esta ready

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,3 +1,6 @@
+const path = require('path');
+
+
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors')
@@ -26,8 +29,9 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-
-
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 
 
